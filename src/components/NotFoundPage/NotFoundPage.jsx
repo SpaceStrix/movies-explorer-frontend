fb709a38e82a74bc368374fa8938f9e3905cc296
Deos <3xiksy@gmail.com>
2023-04-01 12:20:41 +0300

    fix: кнопка назад

--- src/components/NotFoundPage/NotFoundPage.jsx
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -2,15 +2,21 @@ import "./NotFoundPage.css";
 
 import { Link } from "react-router-dom";
 
+import { useNavigate } from "react-router-dom";
+
 export const NotFoundPage = () => {
+  const navigate = useNavigate();
   return (
     <section className="notfoundpage">
       <div className="notfoundpage__section">
         <h1 className="notfoundpage__title">404</h1>
         <p className="notfoundpage__descr">Страница не найдена</p>
-        <Link to={"/"} className="notfoundpage__back">
+        {/* <Link to={"/"} className="notfoundpage__back">
           Назад
-        </Link>
+        </Link> */}
+        <button className="notfoundpage__back" onClick={() => navigate(-1)}>
+          Назад
+        </button>
       </div>
     </section>
   );
